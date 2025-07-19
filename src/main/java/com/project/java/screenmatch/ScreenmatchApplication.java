2564273a88ac9bac95d3ddc03a17b4ae7f1e3503
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-18 23:56:52 -0500

    Update(ScreenmatchApplication.java): Update main method

--- src/main/java/com/project/java/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/project/java/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package com.project.java.screenmatch;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.project.java.screenmatch.service.ConsumoAPI;
+import com.project.java.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +15,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-	@Override
+	@JsonIgnoreProperties(ignoreUnknown = true)
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=highlander&apikey=d33d3103");
 		System.out.println(json);
-
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 }
