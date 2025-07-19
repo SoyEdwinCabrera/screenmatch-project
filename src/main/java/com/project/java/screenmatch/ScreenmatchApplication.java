21813d772275ae6aa4c2b822b53945d951d92b45
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-18 23:02:55 -0500

    feat(ScreenmatchApplication.java): added ScreenmatchApplication class

---
+++ src/main/java/com/project/java/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,23 @@
+package com.project.java.screenmatch;
+
+import com.project.java.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=highlander&apikey=d33d3103");
+		System.out.println(json);
+
+	}
+}
